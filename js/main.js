0b353d1eae031c993bf139cb49213d012f1be44c
alashchev17 <andrew.lashchev15@gmail.com>
2021-07-12 19:08:51 +0300

    create: hotel-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const hotelSwiper = new Swiper(".hotel-slider", {
+	// Optional parameters
+	loop: true,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: ".hotel-slider__button--next",
+		prevEl: ".hotel-slider__button--prev",
+	},
+	grabCursor: true,
+	keyboard: {
+		enabled: true,
+	},
+})
