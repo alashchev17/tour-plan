0b2c27d95bdf88ce4a1d250f6a75215aa9534662
alashchev17 <andrew.lashchev15@gmail.com>
2021-07-14 18:23:05 +0300

    create: added slider in reviews section

--- js/main.js
+++ js/main.js
@@ -11,4 +11,18 @@ const hotelSwiper = new Swiper(".hotel-slider", {
 	keyboard: {
 		enabled: true,
 	},
-})
+});
+const reviewsSwiper = new Swiper(".reviews-slider", {
+	// Optional parameters
+	loop: true,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: ".reviews-slider__button--next",
+		prevEl: ".reviews-slider__button--prev",
+	},
+	grabCursor: true,
+	keyboard: {
+		enabled: true,
+	},
+});
