5ad5a5bd0c7ff507690d8637e6b65256b94cc7b0
alashchev17 <andrew.lashchev15@gmail.com>
2021-07-28 22:38:39 +0300

    fix: responsive fixes and send.php refactoring

--- send.php
+++ send.php
@@ -1,75 +1,59 @@
 <?php
-// Файлы phpmailer
 require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
-
-// Переменные, которые отправляет пользователь
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 $email = $_POST['email'];
 
 if( $email == "" ) {
-  // Формирование самого письма
-  $title = "Новое обращение - Best Tour Plan";
+  $title = "New Appeal - Best Tour Plan";
   $body = "
-  <h2>Новое обращение</h2>
-  <b>Имя:</b> $name<br>
-  <b>Телефон:</b> $phone<br><br>
-  <b>Сообщение:</b><br>$message
+  <h2>New Appeal!</h2>
+  <b>Name:</b> $name<br>
+  <b>Phone Number:</b> $phone<br><br>
+  <b>Message:</b><br>$message
   ";
   header ('Location: thankyou.html');
 } elseif( $email != "" && $name == "" && $phone == "" && $message == "" ){
-  $title = "Запрос на подписку Best Tour plan";
+  $title = "Newsletter subscribtion - Best Tour Plan";
   $body = "
-  <h2>Новое письмо</h2>
-  <b>Электронная почта':</b> $email<br>
-  <span> Данный пользователь хочет подписаться на обновления </span>
+  <h2>New Letter!</h2>
+  <b>Email Address:</b> $email<br>
+  <span> This user wants to subscribe for newsletter mailing. </span>
   ";
-      // Отображение результата
   header ('Location: subscribe.html');    
 } else{
-  $title = "Запрос на бронирование номера - Best Tour plan";
+  $title = "Booking Request - Best Tour plan";
   $body = "
-  <h2>Бронирование номера - Grand Hilton Hotel</h2>
-  <b>Имя:</b> $name<br>
-  <b>Телефон:</b> <a href='tel:$phone'>$phone</a><br>
-  <b>Электронная почта:</b> $email<br><br>
-  <b>Сообщение:</b><br>$message
+  <h2>Room reservation - Grand Hilton Hotel</h2>
+  <b>Name:</b> $name<br>
+  <b>Phone Number:</b> <a href='tel:$phone'>$phone</a><br>
+  <b>Email Address:</b> $email<br><br>
+  <b>Message:</b><br>$message
   ";
   header ('Location: booking.html');
 };
 
-// Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
 try {
     $mail->isSMTP();   
     $mail->CharSet = "UTF-8";
     $mail->SMTPAuth   = true;
-    // $mail->SMTPDebug = 2;
     $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
-
-    // Настройки вашей почты
-    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-    $mail->Username   = 'best.tour.plan.lashchev.dev@gmail.com'; // Логин на почте
-    $mail->Password   = 'cYI1HkHp2D@Z#0Ew&&lS4GEXiu'; // Пароль на почте
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->Username   = 'best.tour.plan.lashchev.dev@gmail.com';
+    $mail->Password   = 'cYI1HkHp2D@Z#0Ew&&lS4GEXiu';
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
-    $mail->setFrom('best.tour.plan.lashchev.dev@gmail.com', 'Best Tour Plan'); // Адрес самой почты и имя отправителя
-
-    // Получатель письма
+    $mail->setFrom('best.tour.plan.lashchev.dev@gmail.com', 'Best Tour Plan');
     $mail->addAddress('andrew.lashchev15@gmail.com');
-
-// Отправка сообщения
 $mail->isHTML(true);
 $mail->Subject = $title;
 $mail->Body = $body;    
-
-// Проверяем отравленность сообщения
 if ($mail->send()) {$result = "success";} 
 else {$result = "error";}
-
 } catch (Exception $e) {
     $result = "error";
     $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
